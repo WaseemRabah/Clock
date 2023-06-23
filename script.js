784762f8e1a27ff55e9e0dea2125a1c07a20ec87
waseem Rabah <wasee.96.ra@gmail.com>
2023-06-24 01:46:08 +0200

    Add values for hand rotation

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 //To call cetrain function every second
 setInterval(setClock, 1000)
 
+// set the values for the rotation of the hands
+const hourHand = document.querySelector('[data-hour-hand]')
+const minuteHand = document.querySelector('[data-minute-hand]')
+const secondHand = document.querySelector('[data-second-hand]')
+
